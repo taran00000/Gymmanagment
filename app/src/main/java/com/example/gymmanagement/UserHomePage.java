00e79301ba8098bc079a06b80894c37819183e1f
angadsingh <imwolv@gmail.com>
2021-02-02 10:01:23 -0500

    First Commit, User page, Trainer List.

--- app/src/main/java/com/example/gymmanagement/UserHomePage.java
+++ app/src/main/java/com/example/gymmanagement/UserHomePage.java
@@ -1,6 +1,9 @@
 package com.example.gymmanagement;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +13,23 @@ public class UserHomePage extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage_user);
+    }
+
+    public static class User_Home_TransformationList extends AppCompatActivity {
+        TextView viewdetails;
+        @Override
+        protected void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.user_home_transformationlist);
+            viewdetails=findViewById(R.id.view_details);
+
+            viewdetails.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent i=new Intent(User_Home_TransformationList.this,User_BodyTransformationDetails.class);
+                    startActivity(i);
+                }
+            });
+        }
     }
 }
--- app/src/main/java/com/example/gymmanagement/UserPage_TrainerList.java
+++ app/src/main/java/com/example/gymmanagement/UserPage_TrainerList.java
@@ -3,23 +3,25 @@ package com.example.gymmanagement;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class User_Home_TransformationList extends AppCompatActivity {
-    TextView viewdetails;
+public class UserPage_TrainerList extends AppCompatActivity {
+    LinearLayout layout;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.user_home_transformationlist);
-        viewdetails=findViewById(R.id.view_details);
+        setContentView(R.layout.userpage_trainerlist);
 
-        viewdetails.setOnClickListener(new View.OnClickListener() {
+        layout=findViewById(R.id.aboutsample);
+
+        layout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i=new Intent(User_Home_TransformationList.this,User_BodyTransformationDetails.class);
+                Intent i=new Intent(UserPage_TrainerList.this,AboutUs.class);
                 startActivity(i);
             }
         });
