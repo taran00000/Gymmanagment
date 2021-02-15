895a1ace862bfc22dd82854235a0ce735648e2bd
suk57 <kaursukhveer1313@gmail.com>
2021-02-15 13:27:18 -0500

    trainer userviewlist

---
+++ app/src/main/java/com/example/gymmanagement/commonlist.java
@@ -0,0 +1,14 @@
+package com.example.gymmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class commonlist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.commonlist);
+    }
+}
---
+++ app/src/main/java/com/example/gymmanagement/trainer_userviewlist.java
@@ -0,0 +1,14 @@
+package com.example.gymmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class trainer_userviewlist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.trainer_userviewlist);
+    }
+}
